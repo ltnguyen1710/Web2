8249f606955a1cca783039f1be184da35cfcb403
NamDan313 <dannguyen.3132001@gmail.com>
2021-05-13 15:00:27 +0700

    phân trang ( T-shirt.php)

--- T-shirt.php
+++ T-shirt.php
@@ -493,14 +493,44 @@ if (isset($_POST["username1"])) {
 
     </div>
 
-    <!-- Product grid -->
+    
+     <!-------------- Phan trang--------------->
+     <?php
+            $conn = createDbConnection();
+                // BƯỚC 2: TÌM TỔNG SỐ RECORDS
+            $result = mysqli_query($conn, 'select count(*) as total from sanpham where maloaisp = 2');
+            $row = mysqli_fetch_assoc($result);
+            $total_records = $row['total'];
+                // BƯỚC 3: TÌM LIMIT VÀ CURRENT_PAGE
+            $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+            $limit = 4;
+                // BƯỚC 4: TÍNH TOÁN TOTAL_PAGE VÀ START
+                // tổng số trang
+            $total_page = ceil($total_records / $limit);
+                // Giới hạn current_page trong khoảng 1 đến total_page
+            if ($current_page > $total_page){
+                 $current_page = $total_page;
+            }
+            else if ($current_page < 1){
+                   $current_page = 1;
+            }
+ 
+                // Tìm Start
+            $start = ($current_page - 1) * $limit;
+
+                // BƯỚC 5: TRUY VẤN LẤY DANH SÁCH TIN TỨC
+                // Có limit và start rồi thì truy vấn CSDL lấy danh sách sản phẩm
+            $result = mysqli_query($conn, "SELECT * FROM sanpham  LIMIT $start, $limit");
+?>
+        
+       <!-- Product grid -->
     <div class="w3-row w3-whitescale" id="myTable">
       <?php
       $conn = createDBConnection();
-      $sql = "SELECT * FROM SANPHAM where maloaisp = 2"; // phân loại sản phẩm
-      $result = $conn->query($sql);
+     
+      $result = mysqli_query($conn, "SELECT * FROM sanpham  where maloaisp = 2 LIMIT $start, $limit");
       while ($row = $result->fetch_assoc()) {
-        
+       
       ?>
 
         <div class="w3-col l3 s6">
@@ -576,6 +606,39 @@ if (isset($_POST["username1"])) {
 
 
     </div>
+        <div class="w3-bar w3-center ">
+           <?php 
+            // PHẦN HIỂN THỊ PHÂN TRANG
+            // BƯỚC 7: HIỂN THỊ PHÂN TRANG
+ 
+            // nếu current_page > 1 và total_page > 1 mới hiển thị nút prev
+            if ($current_page > 1 && $total_page > 1){
+                echo '<a href="T-shirt.php?page='.($current_page-1).'">Prev</a> | ';
+            }
+ 
+            // Lặp khoảng giữa
+            for ($i = 1; $i <= $total_page; $i++){
+                // Nếu là trang hiện tại thì hiển thị thẻ span
+                // ngược lại hiển thị thẻ a
+                if ($i == $current_page){
+                    echo '<span>'.$i.'</span> | ';
+                }
+                else{
+                    echo '<a href="T-shirt.php?page='.$i.'">'.$i.'</a> | ';
+                }
+            }
+ 
+            // nếu current_page < $total_page và total_page > 1 mới hiển thị nút prev
+            if ($current_page < $total_page && $total_page > 1){
+                echo '<a href="T-shirt.php?page='.($current_page+1).'">Next</a> | ';
+            }
+           ?>
+        </div>
+    <!-------------- Phan trang--------------->
+    <div class="w3-container">
+
+
+           
     <!-- Subscribe section -->
     <div class="w3-container w3-black w3-padding-32">
       <h1>Subscribe</h1>
