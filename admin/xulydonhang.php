225cbde99d3fbfa81c09e2045e1c721b719906ae
holakaka <74349746+holakaka@users.noreply.github.com>
2022-11-07 08:41:37 +0700

    dong bo dang nhap database

--- admin/xulydonhang.php
+++ admin/xulydonhang.php
@@ -1,9 +1,5 @@
 <?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "dct119c2";
-$con = new mysqli($servername, $username, $password, $dbname);
+$con = createDBConnection();
 $sql="UPDATE donhang set tinhtrang='Da xu ly' where maDon=".$_REQUEST['madon'];
 $result=$con->query($sql);
 $con->close();
