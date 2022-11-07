8163229b9f16d53f06c70d84f1e050d2110cffcf
holakaka <74349746+holakaka@users.noreply.github.com>
2022-11-07 08:43:18 +0700

    thieu inclue sr sr

--- admin/xulydonhang.php
+++ admin/xulydonhang.php
@@ -1,4 +1,5 @@
 <?php 
+require_once('../process/login.php');
 $con = createDBConnection();
 $sql="UPDATE donhang set tinhtrang='Da xu ly' where maDon=".$_REQUEST['madon'];
 $result=$con->query($sql);
